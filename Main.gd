c001a2c07ed190ec273eda06fa2630178d385561
hvbargen <37015738+hvbargen@users.noreply.github.com>
2022-11-29 20:07:03 +0100

    The camera can now be switched, 3rdPerson shoulder view or fixed camera position - use keyboard C or PS4 triangle button

--- Main.gd
+++ Main.gd
@@ -11,13 +11,23 @@ export (PackedScene) var flower_scene
 const Logger = preload("Logger.gd")
 var logger:= Logger.new("Main", Logger.Level.DEBUG)
 
+var active_cam : int = 0
+
+const camera_nodes: Array = ["Gardener/3rdPerson/Camera", "FixedPerspective1/Camera"]
+var cameras : Array
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var f = $Testflower/Flower
 	f.spawn_leafs(0.5)
 	f.spawn_blossom(1)
 	$"Testpflanze/Blüte".grow(2)
-
+	
+	for node_name in camera_nodes:
+		cameras.append(get_node(node_name))
+	
+	cameras[active_cam].current = true	
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
@@ -31,8 +41,11 @@ func _on_Gardener_spawned_seed():
 	new_seed.initialize(t)
 	add_child(new_seed)
 	new_seed.connect("seed_sunken", self, "_on_Seed_seed_sunken")
-
-
+	
+func _input(event):
+	if event.is_action_pressed("switch_camera"):
+		switch_camera()
+	
 func _on_Seed_seed_sunken(where, color):
 	logger.debug("Seed sunken at %s, color is %s", where, color)
 	var new_flower = flower_scene.instance()
@@ -40,3 +53,6 @@ func _on_Seed_seed_sunken(where, color):
 	add_child(new_flower)
 	new_flower.begin_growing()
 	
+func switch_camera():
+	active_cam = (active_cam+1) % len(cameras)
+	cameras[active_cam].current = true
